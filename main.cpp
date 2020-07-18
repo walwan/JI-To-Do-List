6680436e0b9d65e6c956ca1767ff309f19e85d37
walwan <11062281+walwan@users.noreply.github.com>
2020-07-18 09:00:56 +0800

    Modify .gitignore file. Add initial codes.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+#define WELCOME_MSG "Welcome to JI To-Do List!\nPlease enter your command.\nType \"help\" for help."
+
+int main(){
+    cout << WELCOME_MSG << endl;
+    return 0;
+}
+
