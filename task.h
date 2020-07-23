4b5d91ae737abb819293825a22ec65b2034d9a8c
Jay-04 <62212105+Jay-04@users.noreply.github.com>
2020-07-23 12:45:59 +0800

    Add some constant values and functions used in the suggested list generator. Change the data type of time_cost to int.

--- task.h
+++ task.h
@@ -2,21 +2,29 @@
 #define TASK_H
 
 #define INT_TASK_DUE_SIZE 5
-
+#define INDEX_DUE_MIN 4
+#define INDEX_DUE_HOUR 3
+#define INDEX_DUE_DAY 2
+#define INDEX_DUE_MONTH 1
+#define INDEX_DUE_YEAR 0
 class Task{
     public:
         Task(   std::string new_task_name,
                 std::string new_task_description,
                 int *new_due,
                 int new_priority = 0,
-                double new_time_cost = 0);
+                int new_time_cost = 0);
+        int * time_subtracter();
+        int get_priority() const;
+        std::string get_name();
+        std::string get_description();
     private:
         std::string task_name;
         std::string task_description;
         int due[INT_TASK_DUE_SIZE];
         int priority;
         //A task should cost less than 1000 minutes
-        double time_cost;
+        int time_cost;
 };
 
 /*//Use doubly linked list for task storage
